60f3d8458cd16ac26d2b6660de6ae916392b78e7
tamerhayek <dev@tamerhayek.com>
2024-08-21 09:43:05 +0200

    feat: HTMLNode class and tests

---
+++ src/htmlnode.py
@@ -0,0 +1,18 @@
+class HTMLNode:
+	def __init__(self, tag: str = None, value: str = None, children: list = None, props: dict = None):
+		self.tag = tag
+		self.value = value
+		self.children = children
+		self.props = props
+
+	def to_html(self):
+		raise NotImplementedError
+
+	def props_to_html(self) -> str:
+		texts = []
+		for key, value in self.props.items():
+			texts.append(f"{key}='{value}'")
+		return " ".join(texts)
+
+	def __repr__(self) -> str:
+		return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,41 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+	def test_eq(self):
+		node = HTMLNode("p", "This is a html node")
+		self.assertEqual(node.tag, "p")
+		self.assertEqual(node.value, "This is a html node")
+		self.assertEqual(node.children, None)
+		self.assertEqual(node.props, None)
+	def test_eq_children(self):
+		childrens = [HTMLNode("b", "bold text")]
+		node = HTMLNode("p", "This is a html node", childrens)
+		self.assertEqual(node.tag, "p")
+		self.assertEqual(node.value, "This is a html node")
+		self.assertEqual(node.children, childrens)
+		self.assertEqual(node.props, None)
+	def test_eq_props(self):
+		props = {"class": "bold"}
+		node = HTMLNode("p", "This is a html node", props=props)
+		self.assertEqual(node.tag, "p")
+		self.assertEqual(node.value, "This is a html node")
+		self.assertEqual(node.children, None)
+		self.assertEqual(node.props, props)
+	def test_to_html(self):
+		node = HTMLNode("p", "This is a html node")
+		self.assertRaises(NotImplementedError, node.to_html)
+	def test_props_to_html(self):
+		props = {"href": "https://www.boot.dev", "class": "bold", "target": "_blank"}
+		node = HTMLNode("a", "This is a html node", props=props)
+		self.assertEqual(node.props_to_html(), "href='https://www.boot.dev' class='bold' target='_blank'")
+	def test_repr(self):
+		childrens = [HTMLNode("b", "bold text")]
+		props = {"class": "bold"}
+		node = HTMLNode("p", "This is a html node", childrens, props)
+		self.assertEqual(repr(node), "HTMLNode(p, This is a html node, [HTMLNode(b, bold text, None, None)], {'class': 'bold'})")
+
+if __name__ == "__main__":
+	unittest.main()
