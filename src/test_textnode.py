4befa2a88838a072b692532095b310cd73fb46e1
tamerhayek <dev@tamerhayek.com>
2024-08-21 11:34:05 +0200

    feat: text_node_to_html_node function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from textnode import TextNode
+from leafnode import LeafNode
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -23,6 +24,34 @@ class TestTextNode(unittest.TestCase):
 	def test_repr(self):
 		node = TextNode("This is a text node", "bold", "https://www.boot.dev")
 		self.assertEqual(repr(node), "TextNode(This is a text node, bold, https://www.boot.dev)")
+
+	def test_text_node_to_html_node(self):
+		node = TextNode("This is a text node", TextType.TEXT)
+		leaf = LeafNode(None, "This is a text node")
+		self.assertEqual(repr(text_node_to_html_node(node)), repr(leaf))
+	def test_text_node_to_html_node_bold(self):
+		node = TextNode("This is a bold tag", TextType.BOLD)
+		leaf = LeafNode("b", "This is a bold tag")
+		self.assertEqual(repr(text_node_to_html_node(node)), repr(leaf))
+	def test_text_node_to_html_node_italic(self):
+		node = TextNode("This is a italic tag", TextType.ITALIC)
+		leaf = LeafNode("i", "This is a italic tag")
+		self.assertEqual(repr(text_node_to_html_node(node)), repr(leaf))
+	def test_text_node_to_html_node_code(self):
+		node = TextNode("This is a code tag", TextType.CODE)
+		leaf = LeafNode("code", "This is a code tag")
+		self.assertEqual(repr(text_node_to_html_node(node)), repr(leaf))
+	def test_text_node_to_html_node_link(self):
+		node = TextNode("This is a link tag", TextType.LINK, "https://tamerhayek.com")
+		leaf = LeafNode("a", "This is a link tag", {"href": "https://tamerhayek.com" })
+		self.assertEqual(repr(text_node_to_html_node(node)), repr(leaf))
+	def test_text_node_to_html_node_link(self):
+		node = TextNode("This is a image tag", TextType.IMAGE, "https://tamerhayek.com/avatar.webp")
+		leaf = LeafNode("img", "", {"src": "https://tamerhayek.com/avatar.webp", "alt": "This is a image tag" })
+		self.assertEqual(repr(text_node_to_html_node(node)), repr(leaf))
+	def test_text_node_to_html_node_default(self):
+		node = TextNode("This is a default fallback", "")
+		self.assertRaises(ValueError, text_node_to_html_node, node)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,8 @@
 from enum import Enum
 
+from htmlnode import HTMLNode
+from leafnode import LeafNode
+
 TextType = Enum('TextType', ['TEXT', 'BOLD', 'ITALIC', 'IMAGE', 'LINK', 'CODE'])
 
 class TextNode:
@@ -13,3 +16,21 @@ class TextNode:
 
 	def __repr__(self):
 		return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode) -> HTMLNode:
+	match(text_node.text_type):
+		case TextType.TEXT:
+			return LeafNode(None, text_node.text)
+		case TextType.BOLD:
+			return LeafNode("b", text_node.text)
+		case TextType.ITALIC:
+			return LeafNode("i", text_node.text)
+		case TextType.CODE:
+			return LeafNode("code", text_node.text)
+		case TextType.LINK:
+			return LeafNode("a", text_node.text, {"href": text_node.url})
+		case TextType.IMAGE:
+			return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+		case _:
+			raise ValueError(f"Invalid text type: {text_node.text_type}")
