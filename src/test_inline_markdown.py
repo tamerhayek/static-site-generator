12c3437376d7d5b0469b45a49b057c3e7ae18da4
tamerhayek <dev@tamerhayek.com>
2024-08-21 12:18:01 +0200

    feat: split images and links

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from inline_markdown import extract_markdown_images, extract_markdown_links, split_nodes_delimiter
+from inline_markdown import extract_markdown_images, extract_markdown_links, split_nodes_delimiter, split_nodes_image, split_nodes_link
 from textnode import TextNode, TextType
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -79,6 +79,121 @@ class TestInlineMarkdown(unittest.TestCase):
 				TextNode("This is text with a ", TextType.TEXT),
 				TextNode("code block", TextType.CODE),
 				TextNode(" word", TextType.TEXT),
+			],
+			new_nodes,
+		)
+
+	def test_split_image(self):
+		node = TextNode(
+			"This is text with an ![image](https://tamerhayek.com/avatar.webp)",
+			TextType.TEXT,
+		)
+		new_nodes = split_nodes_image([node])
+		self.assertListEqual(
+			[
+				TextNode("This is text with an ", TextType.TEXT),
+				TextNode("image", TextType.IMAGE, "https://tamerhayek.com/avatar.webp"),
+			],
+			new_nodes,
+		)
+
+	def test_split_image_single(self):
+		node = TextNode(
+			"![image](https://tamerhayek.com/avatar.webp)",
+			TextType.TEXT,
+		)
+		new_nodes = split_nodes_image([node])
+		self.assertListEqual(
+			[
+				TextNode("image", TextType.IMAGE, "https://tamerhayek.com/avatar.webp"),
+			],
+			new_nodes,
+		)
+
+	def test_split_images(self):
+		node = TextNode(
+			"This is text with an ![image](https://tamerhayek.com/avatar.webp) and another ![second image](https://tamerhayek.com/assets/images/portrait.webp)",
+			TextType.TEXT,
+		)
+		new_nodes = split_nodes_image([node])
+		self.assertListEqual(
+			[
+				TextNode("This is text with an ", TextType.TEXT),
+				TextNode("image", TextType.IMAGE, "https://tamerhayek.com/avatar.webp"),
+				TextNode(" and another ", TextType.TEXT),
+				TextNode(
+					"second image", TextType.IMAGE, "https://tamerhayek.com/assets/images/portrait.webp"
+				),
+			],
+			new_nodes,
+		)
+
+	def test_split_no_image(self):
+		node = TextNode(
+			"This is a text with no image",
+			TextType.TEXT,
+		)
+		new_nodes = split_nodes_image([node])
+		self.assertListEqual(
+			[
+				TextNode("This is a text with no image", TextType.TEXT),
+			],
+			new_nodes,
+		)
+
+	def test_split_link(self):
+		node = TextNode(
+			"This is text with a [link](https://tamerhayek.com)",
+			TextType.TEXT,
+		)
+		new_nodes = split_nodes_link([node])
+		self.assertListEqual(
+			[
+				TextNode("This is text with a ", TextType.TEXT),
+				TextNode("link", TextType.LINK, "https://tamerhayek.com"),
+			],
+			new_nodes,
+		)
+
+	def test_split_link_single(self):
+		node = TextNode(
+			"[link](https://tamerhayek.com)",
+			TextType.TEXT,
+		)
+		new_nodes = split_nodes_link([node])
+		self.assertListEqual(
+			[
+				TextNode("link", TextType.LINK, "https://tamerhayek.com"),
+			],
+			new_nodes,
+		)
+
+	def test_split_links(self):
+		node = TextNode(
+			"This is text with a [link](https://tamerhayek.com) and [another link](https://tamerhayek.com/github) with text that follows",
+			TextType.TEXT,
+		)
+		new_nodes = split_nodes_link([node])
+		self.assertListEqual(
+			[
+				TextNode("This is text with a ", TextType.TEXT),
+				TextNode("link", TextType.LINK, "https://tamerhayek.com"),
+				TextNode(" and ", TextType.TEXT),
+				TextNode("another link", TextType.LINK, "https://tamerhayek.com/github"),
+				TextNode(" with text that follows", TextType.TEXT),
+			],
+			new_nodes,
+		)
+
+	def test_split_no_link(self):
+		node = TextNode(
+			"This is a text with no link",
+			TextType.TEXT,
+		)
+		new_nodes = split_nodes_link([node])
+		self.assertListEqual(
+			[
+				TextNode("This is a text with no link", TextType.TEXT),
 			],
 			new_nodes,
 		)
